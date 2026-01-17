5c747e4bdd31bc3e7c68e6e751349cf4f76f2fc2
DustyTalent <149543139+DustyTalent@users.noreply.github.com>
2026-01-17 05:52:58 -0700

    Refactor cookie clicker script with new parameters

--- cheatCookieClicker.py
+++ cheatCookieClicker.py
@@ -1,50 +1,22 @@
-import pyautogui as pen
+import pyautogui as pygoo
 import sys
 
-pen.moveTo(2200, 400, 0.5)
+pygoo.moveTo(0, 0, 0.5) #Your coordinates here
 
-corner_x1 = 1930
-corner_x2 = 2500
-corner_y1 = 160
-corner_y2 = 1000
+corner_x1 = 0 #Put in the area you want to click in these 4 variables
+corner_x2 = 0 
+corner_y1 = 0
+corner_y2 = 0
 
 try:
     while True:
-        for i in range (5510):
-            pen.click()
+        for i in range (0):
+            pygoo.click()
 
             if i == 100:
                 print(f'{i} cookies clicked')
             elif i == 500:
                 print('500 cookies clicked')
-            elif i == 800:
-                print('800 cookies clicked')
-            elif i == 1000:
-                print('1000 cookies clicked')
-            elif i == 1500:
-                print(f'{i} cookies clicked')
-            elif i == 2000:
-                print(f'{i} cookies clicked')
-            elif i == 2500:
-                print(f'{i} cookies clicked')
-            elif i == 3000:
-                print(f'{i} cookies clicked')
-            elif i == 4000:
-                print(f'{i} cookies clicked')
-            elif i == 5000:
-                print(f'{i} cookies clicked')
-            elif i == 6000:
-                print(f'{i} cookies clicked')
-            elif i == 7000:
-                print(f'{i} cookies clicked')
-            elif i == 8000:
-                print(f'{i} cookies clicked')
-            elif i == 9000:
-                print(f'{i} cookies clicked')
-            elif i == 10000:
-                print('10000 cookies clicked program complete')
-            elif i > 10001:
-                print('Too many cookies something has gone terribly WRONG')
 
             x, y = pen.position()
             if corner_x1<= x <= corner_x2 and corner_y1 <= y <= corner_y2:
@@ -52,12 +24,13 @@ try:
                 continue
             else:
                 m = False
-                if m == False:
+                if m == False: #The program will go CRAZY without this, or else it constantly auto clicks everything
                     print('Program ended since mouse left the area')
                     sys.exit()
                 else:
                     continue
                 
 
-except KeyboardInterrupt:
+except KeyboardInterrupt: #Ctrl-C to end program, as per usual
     print('Program Ended\n')
+
